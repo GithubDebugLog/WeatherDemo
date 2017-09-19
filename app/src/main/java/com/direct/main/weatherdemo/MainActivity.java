9d2db94ef76ab0812b8086599c4b7bd83f676f41
heqiang <18798840027@163.com>
2017-09-19 14:34:17 +0800

    增加了一些包

--- app/src/main/java/com/direct/main/weatherdemo/MainActivity.java
+++ app/src/main/java/com/direct/main/weatherdemo/MainActivity.java
@@ -10,5 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //https://github.com/GithubDebugLog/MyFirstDemo.git
 }
